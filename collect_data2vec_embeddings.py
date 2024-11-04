e7da81527fe5b4443e8f5c4e11653554878529dd
X. Yang <xy316@cam.ac.uk>
2024-11-04 09:27:02 +0000

    finish embedding extraction for data2vec

--- collect_data2vec_embeddings.py
+++ collect_data2vec_embeddings.py
@@ -1,13 +1,44 @@
+import argparse
+import logging
+
 import torch
 import numpy as np
 
 from lhotse import load_manifest_lazy, CutSet
+from lhotse.utils import fastcopy
 from lhotse.cut import MonoCut
-from lhotse.features.io import NumpyHdf5Writer
+from lhotse.features.io import LilcomChunkyWriter
 from torch.utils.data import DataLoader
 from lhotse.dataset import DynamicBucketingSampler, UnsupervisedWaveformDataset
 
 from transformers import AutoProcessor, Data2VecAudioModel
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    
+    parser.add_argument(
+        "--data2vec-version",
+        type=str,
+        choices=["large", "base"],
+        required=True,
+    )
+    
+    parser.add_argument(
+        "--layer-idx",
+        type=int,
+        default=-1,
+        help="The index starts from 1, so if you want the 12-th layer feature, just set it to 12"
+    )
+    
+    parser.add_argument(
+        "--subset",
+        type=str,
+        required=True,
+    )
+    
+    return parser.parse_args()
 
 def make_pad_mask(lengths: torch.Tensor, max_len: int = 0) -> torch.Tensor:
     """
@@ -55,10 +86,17 @@ def test_data2vec():
     assert torch.all(outputs.hidden_states[-1] == outputs.last_hidden_state)
     
 @torch.no_grad()
-def collect_results(manifest_path, embedding_path, layer_idx=21, max_duration=200):
+def collect_results(
+    model_version,
+    manifest_path,
+    embedding_path,
+    output_manifest_path,
+    layer_idx=21,
+    max_duration=200  
+):
     # load the pre-trained checkpoints
-    processor = AutoProcessor.from_pretrained("facebook/data2vec-audio-base-960h")
-    model = Data2VecAudioModel.from_pretrained("facebook/data2vec-audio-base-960h")
+    processor = AutoProcessor.from_pretrained(f"facebook/data2vec-audio-{model_version}")
+    model = Data2VecAudioModel.from_pretrained(f"facebook/data2vec-audio-{model_version}")
     model.eval()
     
     manifest = load_manifest_lazy(manifest_path)
@@ -86,7 +124,7 @@ def collect_results(manifest_path, embedding_path, layer_idx=21, max_duration=20
     
     new_cuts = []
     num_cuts = 0
-    with NumpyHdf5Writer(embedding_path) as writer:
+    with LilcomChunkyWriter(embedding_path) as writer:
         for i, batch in enumerate(dl):
             cuts = batch["cuts"]
             audios = []
@@ -117,18 +155,16 @@ def collect_results(manifest_path, embedding_path, layer_idx=21, max_duration=20
             embedding_lens = padding_mask.sum(dim=-1)
             
             for j, cut in enumerate(cuts):
-                new_cut = MonoCut(
-                    id=cut.id,
-                    start=cut.start,
-                    duration=cut.duration,
-                    channel=cut.channel,
-                )
-                new_cut.data2vec_embedding = writer.store_array(
+                data2vec_embedding = writer.store_array(
                     key=cut.id,
                     value=layer_results[j, :embedding_lens[j]],
                     temporal_dim=0,
                     frame_shift=0.02,
                     start=0,
+                )
+                new_cut = fastcopy(
+                    cut,
+                    custom={"data2vec_embedding": data2vec_embedding}
                 )
                 new_cuts.append(new_cut)
                 num_cuts += 1
@@ -136,18 +172,29 @@ def collect_results(manifest_path, embedding_path, layer_idx=21, max_duration=20
                     print(f"Cuts processed until now: {num_cuts}")
     
     new_cuts = CutSet.from_cuts(new_cuts)
-    new_cuts.to_jsonl(f"manifests/dev-clean-data2vec-base-layer-{layer_idx}.jsonl.gz")
+    new_cuts.to_jsonl(output_manifest_path)
+    logging.info(f"Manifest saved to {output_manifest_path}")
             
         
 if __name__=="__main__":
-    manifest_path = "data/fbank/librispeech_cuts_dev-clean.jsonl.gz"
-    embedding_path = "data2vec_embeddings/data2vec-base-dev-clean.h5"
+    formatter = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    logging.basicConfig(format=formatter, level=logging.INFO)
+    
+    args = get_parser()
+    data2vec_version = args.data2vec_version
+    layer_idx = args.layer_idx
+    subset = args.subset
+    
+    manifest_path = f"data/fbank/librispeech_cuts_{subset}.jsonl.gz"
+    embedding_path = f"embeddings/data2vec_embeddings/data2vec-{data2vec_version}-layer-{layer_idx}-{subset}.h5"
+    output_manifest_path = f"manifests/{subset}-data2vec-{data2vec_version}-layer-{layer_idx}.jsonl.gz"
     max_duration = 100
     
-    # test_data2vec()
     collect_results(
+        model_version=data2vec_version,
         manifest_path=manifest_path,
         embedding_path=embedding_path,
-        layer_idx=12,
+        layer_idx=layer_idx,
         max_duration=max_duration,
+        output_manifest_path=output_manifest_path,
     )
